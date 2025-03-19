92938690269df8fbf7af59751d6a618708bfa589
PtrkLee <patrik.lee@hotmail.com>
2025-03-19 18:52:53 -0500

    Deleted unused files.
    Deleted unused files.

--- myDemo/src/main/java/com/example/myDemo/workerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.myDemo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface workerRepository extends JpaRepository<worker, Integer> {
-    // You can add custom queries here if needed
-}
